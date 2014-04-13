98ec809a90f914d51ecbd773345ba482b6840d00
Barry Quigley <barryquigley@yahoo.com>
2014-04-13 19:29:48 +0100

    222

--- app.rb
+++ app.rb
@@ -22,21 +22,21 @@ module Katana
       end
 
       get '/' do
-        "Shorten all the URLs"
+        render template: "views/index"
       end
 
-      # if ENV['TWEETBOT_API']
-      #   # experimental (unauthenticated) API endpoint for tweetbot
-      #   get '/api/create/?' do
-      #     status, head, body = settings.service.create(params[:url], params[:code])
+      if ENV['TWEETBOT_API']
+        # experimental (unauthenticated) API endpoint for tweetbot
+        get '/api/create/?' do
+          status, head, body = settings.service.create(params[:url], params[:code])
 
-      #     if loc = head['Location']
-      #       "#{File.join("http://", request.host, loc)}"
-      #     else
-      #       500
-      #     end
-      #   end
-      # end
+          if loc = head['Location']
+            "#{File.join("http://", request.host, loc)}"
+          else
+            500
+          end
+        end
+      end
 
       # helper methods
       helpers do
