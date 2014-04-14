86b94659a9964fa2d4f8219825637797fda504e5
Barry Quigley <barryquigley@yahoo.com>
2014-04-14 10:30:34 +0100

    blah

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ module Katana
         "FRAGD URL SHORTENER"
       end
 
-      post '/shorten/:token/:url/:code' do
+      post do
         @@logger.info "<<<<<<<<<<<< shorten <<<<<<<<<<<<<"
 
         status, head, body = settings.service.create(params[:url], params[:code])
