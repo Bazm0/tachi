c1bf771d6d07dec068da1e73ab5ca4a663956934
Barry Quigley <barryquigley@yahoo.com>
2014-04-14 10:54:30 +0100

    blah

--- app.rb
+++ app.rb
@@ -28,14 +28,15 @@ module Katana
       post '/shorten' do
         @@logger.info "<<<<<<<<<<<< shorten <<<<<<<<<<<<<"
 
-        status, head, body = settings.service.create("http://dev01.dev:3000/post/-JIPiq1k6yekkMPjM_mi-1395239119214", "abcdefghijklmnopqrst")
+        status, head, body = settings.service.create("http://dev01.dev:3000/post/-JIPiq1k6yekkMPjM_mi-1395239119214", "abcdefghijklmnopqrstuvw")
 
         @@logger.info "<<<<<<<<<<<< result <<<<<<<<<<<<< status: #{status} ---- head: #{head} ---- body: #{body}"
 
-        respond_to do |format|
-          state = loc == head['Location'] ? 200 : 500
-          format.json { render :json => state }
-        end
+
+        state = loc == head['Location'] ? 200 : 500
+        {status: state}
+
+
 
         # if loc = head['Location']
         #   "#{File.join("http://", request.host, loc)}"
